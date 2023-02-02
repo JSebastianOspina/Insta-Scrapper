81dd89ad1d274fc315b48f67039147019495f5ee
Sebastian Ospina <sebastianospina@Mac-mini-de-Sebastian.local>
2023-02-01 23:42:36 -0500

    update port

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const { getUserInfo } = require('./scrapper');
 
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3030;
+
 
 app.get('/getUser', async (req, res) => {
     const userInfo = await getUserInfo(req.query.user);
     res.send(`<pre id='json'>${JSON.stringify(userInfo, null, 2)}</pre>`);
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
 })
